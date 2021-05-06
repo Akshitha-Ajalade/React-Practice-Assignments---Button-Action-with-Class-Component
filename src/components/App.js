2e309d8f30a428e1a7442863e936580c18b28b2e
Akshitha-Ajalade <81165727+Akshitha-Ajalade@users.noreply.github.com>
2021-05-06 13:38:20 +0530

    updated code

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,24 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    constructor(props) {
-		super(props);
-	};
+  constructor(props) {
+    super(props);
+    this.state = {
+      message:
+        "class components of react and state management in class components"
+    };
+  }
 
-    render() {
-    	return(
-    		<div id="main">
-				{ /* Do not remove this main div!! */ }
-    		</div>
-    	);
-    }
+  render() {
+    return (
+      <div id="main">
+        <button id="click" onClick=<p id="para">{this.state.message}</p>>
+          click me
+        </button>
+      </div>
+    );
+  }
 }
 
-
 export default App;
-
